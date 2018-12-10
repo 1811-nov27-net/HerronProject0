4721a1dc8f6d822580255d8708e63730f2a7337d
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-10 09:11:44 -0600

    Work on console interface.

--- PizzaConsole/Program.cs
+++ PizzaConsole/Program.cs
@@ -8,21 +8,53 @@ namespace PizzaConsole
     {
         static void Main(string[] args)
         {
-            // load data
-            string username, password, answer;
 
-            Console.WriteLine("Please enter your username:");
-            username = Console.ReadLine();
-            Console.WriteLine("Password:");
-            password = Console.ReadLine();
+            string userInput;
+            Console.WriteLine("Login (l) or Quit (q)");
+            userInput = Console.ReadLine();
+            while (userInput[1] != 'q')
+            {
 
-            // find customer
-            Customer customer = new Customer(username, password);
+                // load data
+                string username, password, answer;
 
-            Order CurrentOrder = new Order(customer, password);
+                Console.WriteLine("Please enter your username:");
+                username = Console.ReadLine();
+                Console.WriteLine("Password:");
+                password = Console.ReadLine();
+
+                // find customer
+                Customer customer = new Customer(username, password);
+
+                Order CurrentOrder = new Order(customer, password);
+                bool placeOrder = false, quitLoop = false;
+                while (placeOrder == false && quitLoop == false)
+                {
+                    Console.WriteLine($"Your order currently contains {CurrentOrder.pizzas.Count} pizzas.");
+                    Console.WriteLine("Would you like to (a)dd a pizza, (r)emove a pizza, (p)lace your order or (c)ancel your order?");
+                    answer = Console.ReadLine();
+
+
+                }
+
+                Console.WriteLine("Enter (q) to quit, anything else to continue");
+                userInput = Console.ReadLine();
+
+            }
 
 
 
+        }
+
+        public Pizza AddPizza(Order order)
+        {
+            Console.WriteLine("Size: ");
+            foreach (var size in Enum.GetValues(typeof(Pizza.PizzaSize)))
+            {
+                Console.WriteLine($"{size}: {size.ToString()}");
+            }
+
+            return new Pizza(size,ingrediants);
 
         }
     }
